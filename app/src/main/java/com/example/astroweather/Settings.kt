b75fc8776a5a214ea3572ca32d8b9c464bf41041
Piotr Janek <justfushi@gmail.com>
2019-05-21 19:19:55 +0200

    Code refactor.

--- app/src/main/java/com/example/astroweather/Settings.kt
+++ app/src/main/java/com/example/astroweather/Settings.kt
@@ -22,11 +22,11 @@ class Settings : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    lateinit var editLatitude : TextView
-    lateinit var editLongitude : TextView
-    lateinit var spinnerTime : Spinner
-    lateinit var buttonSaveAndExit : Button
-    lateinit var buttonExitWithoutSaving : Button
+    private lateinit var editLatitude : TextView
+    private lateinit var editLongitude : TextView
+    private lateinit var spinnerTime : Spinner
+    private lateinit var buttonSaveAndExit : Button
+    private lateinit var buttonExitWithoutSaving : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,12 +34,12 @@ class Settings : AppCompatActivity(), View.OnClickListener {
         supportActionBar!!.hide()
         initComponents()
     }
-    fun initComponents(){
+    private fun initComponents(){
         editLatitude = findViewById(R.id.editLatitude)
         editLongitude = findViewById(R.id.editLongitude)
         spinnerTime = findViewById(R.id.spinnerTime)
-        var array_adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, resources.getStringArray(R.array.spinnerAdapter))
-        spinnerTime.adapter = array_adapter
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, resources.getStringArray(R.array.spinnerAdapter))
+        spinnerTime.adapter = arrayAdapter
         buttonSaveAndExit = findViewById(R.id.buttonSaveAndExit)
         buttonExitWithoutSaving = findViewById(R.id.buttonExitWithoutSaving)
     }
